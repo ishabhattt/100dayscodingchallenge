334be8585b7c8252c5c864bf720d05322654199a
ishabhattt <bhattisha000@gmail.com>
2024-04-05 09:54:41 +0530

    set zeros metrix solution

---
+++ set-metrix-zeros/setMetrixZeros.js
@@ -0,0 +1,31 @@
+function setZerosMatrix(arr) {
+  const rows = new Set();
+  const cols = new Set();
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      if (arr[i][j] === 0) {
+        rows.add(i);
+        cols.add(j);
+      }
+    }
+  }
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      if (rows.has(i) || cols.has(j)) {
+        arr[i][j] = 0;
+      }
+    }
+  }
+  return arr;
+}
+
+// Example usage:
+const matrix = [
+  [0, 1, 2, 0],
+  [3, 4, 5, 2],
+  [1, 3, 1, 5],
+];
+console.log(setZerosMatrix(matrix));
+
+// time complexity O(m*n)
+//space complexity O(m+n)
